31154ec444350d7eced4a5f780fd13c224fec2e8
Lavlesh Mishra <lavleshmishra6@gmail.com>
2022-06-29 17:20:12 +0530

    update large button

--- src/components/dropshipbutton/Largebutton.js
+++ src/components/dropshipbutton/Largebutton.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { TouchableOpacity, Text, View } from 'react-native';
 import tw from 'twrnc';
+import { useTailwind } from 'tailwind-rn';
 
 const Largebutton = ({text, onPress}) => {
-
+    const tailwind = useTailwind();
     return (
       <View style={tailwind('items-center')}>
         <TouchableOpacity style={tailwind('items-center w-11/12 py-4 border border-transparent font-medium rounded-full text-white bg-red-800')}
